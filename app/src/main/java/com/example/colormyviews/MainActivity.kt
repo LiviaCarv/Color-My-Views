845cbc9db65f491f4587ba9997f2b9058f562cee
Livia Carvalho <livia.carvalho.lb@gmail.com>
2024-02-05 21:19:26 -0400

    adding specific method for buttons on clickListener

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,10 +17,27 @@ class MainActivity : AppCompatActivity() {
 
     private fun setListeners() {
         val clickableViews: List<View> = listOf(txt_box_one,txt_box_two,txt_box_three,
-            txt_box_four,txt_box_five,constraintLayout)
+            txt_box_four,txt_box_five,constraintLayout,btn_green,btn_red,btn_yellow)
         for (view in clickableViews){
-            view.setOnClickListener{
-                makeItColored(it)
+            if (view is Button) {
+                view.setOnClickListener { colorAllViews(it,clickableViews) }
+            } else {
+                view.setOnClickListener{ makeItColored(it) }
+            }
+
+        }
+    }
+
+    private fun colorAllViews(view: View, viewsList: List<View> ) {
+        when(view.id) {
+            R.id.btn_green -> viewsList.forEach{
+                view.setBackgroundColor(ContextCompat.getColor(this,R.color.btn_green))
+            }
+            R.id.btn_red -> viewsList.forEach {
+                view.setBackgroundColor(ContextCompat.getColor(this,R.color.btn_red))
+            }
+            R.id.btn_yellow -> viewsList.forEach {
+                view.setBackgroundColor(ContextCompat.getColor(this,R.color.btn_yellow))
             }
         }
     }
